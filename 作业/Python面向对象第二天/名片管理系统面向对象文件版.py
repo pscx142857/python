8d7e7acc593b2f87f303398e5a5b62c330475704
pengsheng <pscx142857@163.com>
2021-05-01 14:18:40 +0800

    20210501 名片管理系统加强(最终)

--- 作业/Python面向对象第二天/名片管理系统面向对象文件版.py
+++ 作业/Python面向对象第二天/名片管理系统面向对象文件版.py
@@ -6,7 +6,7 @@
 
 """
 
-
+# 保存数据到data.txt
 def save(cards):
     ls2 = list()
     # 打开文件
@@ -19,11 +19,17 @@ def save(cards):
     # 关闭文件
     fp.close()
 # 定义一个名片类
-
-
 class Card(object):
     # 初始化方法里面写名片的属性,添加属性
     def __init__(self, name, tel, company, job, address):
+        """
+        给名片对象添加属性
+        :param name: 姓名
+        :param tel: 电话
+        :param company: 公司
+        :param job: 职位
+        :param address: 地址
+        """
         self.name = name
         self.tel = tel
         self.company = company
@@ -32,17 +38,27 @@ class Card(object):
     # 打印对象的时候执行的方法,可以写展示名片的信息
 
     def __str__(self):
+        """
+        打印名片对象的时候显示名片信息
+        :return: 返回名片上面的所有信息
+        """
         return f"{self.name}\t\t{self.tel}\t\t{self.company}\t\t{self.job}\t\t{self.address}"
 # 定义一个名片盒子类
-
-
 class Cards(object):
     # 初始化方法里面写添加属性,这个ls是个列表,可以装多个名片对象
     def __init__(self, ls):
+        """
+        给名片盒子对象添加属性
+        :param ls: 一个可以装东西的列表
+        """
         self.ls = ls
     # 添加名片的方法
-
     def add(self, ls):
+        """
+        添加名片
+        :param ls: 装有名片对象的列表
+        :return:
+        """
         # 让用户输入要添加的信息
         name = input("请输入姓名:")
         tel = input("请输入姓名:")
@@ -56,9 +72,14 @@ class Cards(object):
         ls.append(new_card)
         self.ls = ls
         print("恭喜你添加成功")
+        # 调用保存数据函数
         save(self)
     # 展示名片的方法
     def show(self):
+        """
+        展示名片盒子里面的所有名片
+        :return:
+        """
         # 表头
         print("姓名\t\t电话\t\t\t\t公司\t\t\t职位\t\t地址")
         for card in self.ls:
@@ -66,6 +87,10 @@ class Cards(object):
 
     # 修改名片的方法
     def reverse(self):
+        """
+        修改名片
+        :return:
+        """
         # 让用户输入要修改名片的姓名
         name = input("请输入要修改名片的姓名:")
         # 根据这个姓名去匹配是否有这张名片,有-->修改,没有-->提示没有此人
@@ -83,6 +108,7 @@ class Cards(object):
                 card.job = job
                 card.address = address
                 print("恭喜你修改成功")
+                # 调用保存数据的函数
                 save(self)
                 break
         else:
@@ -90,6 +116,11 @@ class Cards(object):
 
     # 删除名片的方法
     def delete(self, ls):
+        """
+        删除名片
+        :param ls: 装有名片对象的列表
+        :return:
+        """
         # 让用户输入要删除名片的姓名
         name = input("请输入要删除名片的姓名:")
         # 根据这个姓名去匹配是否有这张名片,有-->删除;没有-->提示没有此人
@@ -103,8 +134,6 @@ class Cards(object):
                 break
         else:
             print("对不起,没有此人")
-# 保存数据的函数
-
 
 """
     数据准备
@@ -129,7 +158,6 @@ for i in new_st.split("\n"):
     ls.append(card)
 # 创建一个名片盒子类
 cards = Cards(ls)
-
 # 主程序,死循环,一直执行
 while True:
     # 根据用户选择的功能执行对应的方法
